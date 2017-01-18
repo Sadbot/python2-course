f9319c1ee7c0773617d9114098bf55ee112f18e3
sadbot <semseriou@gmail.com>
2017-01-18 21:40:42 +0400

    переписал на P3, исправил регулярки, рефакторинг

--- findnbold.py
+++ findnbold.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python
-#
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-from io import open
-# import re
+import re
 import sys
 
 """There are 2 files - file_with_text and file_dictionary
@@ -13,7 +10,7 @@ file_dictionary contains an arbitrary number of lines, each containing exactly o
 """
 
 
-def update_text(searched_set, text_fname, update_func):
+def update_text(searched_set, text_fname):
     """Create an html file with text in which every word that is searched updating
 
     :param searched_set: Set of searched words
@@ -21,18 +18,16 @@ def update_text(searched_set, text_fname, update_func):
     :return: returns nothing
     """
     with open(text_fname) as f, open('index.html', 'w', encoding='utf-8') as index:
-        index.write(u'<html><head><meta charset="utf-8"/></head><body>\n')
+        index.write('<html><head><meta charset="utf-8"/></head><body>\n')
 
         for line in f:
             replaced_line = ''
-
             for word in searched_set:
-                # replaced_line = re.sub("\\b" + word + "\\b", update_func(word), line)
-                replaced_line = line.replace(word, update_func(word))
-
+                regex = re.compile(r'\b' + word + r'\b', re.UNICODE)
+                replaced_line = regex.sub('<b>' + word + '</b>', line)
             index.write(replaced_line + '<br/>')
 
-        index.write(u'\n</body></html>\n')
+        index.write('\n</body></html>')
 
 
 def read_set(dict_fname):
@@ -42,25 +37,14 @@ def read_set(dict_fname):
     :return: set() of searched words
     """
     searched_set = set()
-
     with open(dict_fname) as input_file:
         for line in input_file:
             searched_set.add(line.strip())
-
     return searched_set
-
-
-def bold_word(word):
-    """Replace word with bolded html alternative"""
-    return "<b>" + word + "</b>"
 
 
 def main():
     args = sys.argv[1:]
-
-    if not args:
-        print('usage: dictionary_file text_file ')
-        sys.exit(1)
 
     if not args[0] or not args[1]:
         print('usage: dictionary_file text_file ')
@@ -72,7 +56,7 @@ def main():
 
     searched_set = read_set(dict_fname)
     if searched_set:
-        update_text(searched_set, text_fname, bold_word)
+        update_text(searched_set, text_fname)
     else:
         print('No searched words')
 
